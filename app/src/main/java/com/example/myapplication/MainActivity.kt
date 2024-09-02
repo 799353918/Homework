57fc056f43d2833c3f67c50515d3deff4f7c2ea3
碧翠斯卡洛儿 <799353918@qq.com>
2024-09-02 19:30:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // 找到文本视图和按钮
+        val textView: TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+
+        // 设置按钮点击事件监听器
+        button.setOnClickListener {
+            textView.text = resources.getString(R.string.clickHere)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
